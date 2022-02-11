065a7acff572c48b4bbdd21ba5e6bd94b5da8c6d
toshimana <toshi.mana.plus1@gmail.com>
2022-02-11 21:54:48 +0900

    ヒント入力部を仮実装

---
+++ WordleHelper/HintKey.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WordleHelper
+{
+    /// <summary>
+    /// Interaction logic for HintKey
+    /// </summary>
+    public partial class HintKey : UserControl
+    {
+        public HintKey()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WordleHelper/HintKeyBoard.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WordleHelper
+{
+    /// <summary>
+    /// Interaction logic for HintKeyBoard
+    /// </summary>
+    public partial class HintKeyBoard : UserControl
+    {
+        public HintKeyBoard()
+        {
+            InitializeComponent();
+        }
+    }
+}

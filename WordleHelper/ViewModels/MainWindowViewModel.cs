8fe1ddda7bdc8d9984ec05d8ed590a686066de75
toshimana <toshi.mana.plus1@gmail.com>
2022-02-11 19:34:03 +0900

    辞書の読み込みを実装

--- WordleHelper/ViewModels/MainWindowViewModel.cs
+++ WordleHelper/ViewModels/MainWindowViewModel.cs
@@ -1,19 +1,24 @@
 ﻿using Prism.Mvvm;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WordleHelper.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
-        private string _title = "Prism Application";
+        private string _title = "Wordle Helper";
         public string Title
         {
             get { return _title; }
             set { SetProperty(ref _title, value); }
         }
 
+        private string[] fivewords = null;
+
         public MainWindowViewModel()
         {
-
+            var words = System.IO.File.ReadLines(@"words_alpha.txt");
+            this.fivewords = words.Where(x => x.Length == 5).ToArray();
         }
     }
 }
